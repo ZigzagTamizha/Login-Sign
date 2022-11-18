ba90a2f1ba8409df3bbdf443b1d7486dcecc722c
ZigzagTamizha <118274991+ZigzagTamizha@users.noreply.github.com>
2022-11-18 21:41:43 +0530

    12 file added

---
+++ LoginPage/index1.js
@@ -0,0 +1,20 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+let password = document.getElementById("password").value;
+let confirm = document.getElementById(password,confirmpassword).value;
+console.log(password,confirmpassword);
+signupBtn.onclick = (()=>{
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+    signupBtn.click();
+    return false;
+});
